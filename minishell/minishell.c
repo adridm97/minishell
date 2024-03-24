0f97bfdb24b17fb189449dce1438e2639678e48d
KevinPortatil <kkevinlunaa95@gmail.com>
2024-03-24 21:54:11 +0100

    Starting project

---
+++ minishell/minishell.c
@@ -0,0 +1,15 @@
+
+#include "minishell.h"
+
+int main(int argc, char *argv[], char *env[])
+{
+    static char *input;
+
+    while(1)
+    {
+        input = readline("\x1b[34m""Minishell:""\x1b[0m");
+        if(!strcmp(input, "exit"))
+            break ;
+        printf("%s\n",input);
+    }
+}
