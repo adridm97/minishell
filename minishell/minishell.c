c8773e47cbab0cf46000b6437e89d778ef2b8168
Adrian Duenas Moreira <aduenas-@car12s1.42barcelona.com>
2024-03-25 22:13:48 +0100

    Structure in .h

--- minishell/minishell.c
+++ minishell/minishell.c
@@ -1,6 +1,18 @@
 
 #include "minishell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
+int es_comando_valido(char *comando) {
+    // Aquí podrías implementar una lógica para verificar si el comando es válido
+    // Por ejemplo, verificar si el archivo ejecutable existe en el sistema
+    if (access(comando, X_OK) != -1) {
+        return 1;
+    }
+    return 0;
+}
 int main(int argc, char *argv[], char *env[])
 {
     static char *input;
@@ -10,6 +22,14 @@ int main(int argc, char *argv[], char *env[])
         input = readline("\x1b[34m""Minishell:""\x1b[0m");
         if(!strcmp(input, "exit"))
             break ;
-        printf("%s\n",input);
+        //printf("%s\n",input);
+ 		if (!es_comando_valido(input)) {
+            printf("Comando inválido: %s\n", input);
+  //          return 1;
+        }
+//		if (execve(input, (char **){"touch", "test.txt", NULL}, env) == -1) {
+  //      perror("Error al ejecutar el comando");
+    //    return 1;
+//    }		
     }
 }
--- minishell/minishell.h
+++ minishell/minishell.h
@@ -6,4 +6,39 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
+//TODO ahora son int
+# define PIPE		"|"
+# define MAJOR		">"
+# define MINOR		"<"
+# define D_MAJOR	">>"
+# define D_MINOR	"<<"
+# define DOLLAR		"$"
+
+typedef struct s_mini{
+    char *path;
+    t_error *errors;
+	t_token *token;
+} t_mini;
+
+typedef struct s_error{
+	char	*error;
+	int		is_error;
+} t_error;
+
+// cuando encontramos un caracter especial la key sera special y el value la macro del caracter
+// cuando sea un comando en key pondremos el comando y en value la continuacion hasta el caracter especial
+typedef struct s_token{
+	int		*key;
+	char	*value;
+	t_token	*next;
+	t_data	*data;
+} t_token;
+
+// args ha de uncluir NULL su ultima posicion
+typedef struct	s_data{
+	t_token	*token;
+	char	*comand;
+	char	*args[];
+} t_data;
+
 #endif
