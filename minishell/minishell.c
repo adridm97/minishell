f6659eb5e07405caefeefe1bd658bb3ceb7b5cff
Kevin Luna Botey <kluna-bo@car10s1.42barcelona.com>
2024-03-29 10:58:54 +0100

    modificate struct s_data

--- minishell/minishell.c
+++ minishell/minishell.c
@@ -5,8 +5,21 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[], char *env[])
+void	parser(char *input, t_data *data)
 {
+
+}
+
+int	main(int argc, char *argv[], char *env[])
+{
+	static char	*input;
+	t_data		*data;
+
+	while (1)
+	{
+		input = readline("\x1b[34m""Minishell:""\x1b[0m");
+		parser(input, data);
+	}
 	return (0);
 }
 
