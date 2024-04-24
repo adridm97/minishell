48f5b7fcbd400c9cb02cbfee7c3eca0e96df32ab
Adrian Duenas Moreira <aduenas-@car9s2.42barcelona.com>
2024-04-24 23:59:22 +0200

    redirecciones hechas menos heredoc

--- minishell/executor.c
+++ minishell/executor.c
@@ -6,7 +6,7 @@
 /*   By: kevin <kevin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 15:42:09 by aduenas-          #+#    #+#             */
-/*   Updated: 2024/04/21 03:15:49 by aduenas-         ###   ########.fr       */
+/*   Updated: 2024/04/24 23:58:08 by aduenas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,98 +32,26 @@ int	ft_strcmp(const char *s1, const char *s2)
 	return (0);
 }
 
-
-static void	sig_handler_def(int signum)
-{
-	if (signum == CTRL_C)
-	{
-		printf("\n");
-		//rl_replace_line("", 1);
-		rl_on_new_line();
-		rl_redisplay();
-	}
-}
-
-static void	sig_handler_childs(int signum)
-{
-	if (signum == CTRL_C)
-		printf("\n");
-}
-
-int	init_signals(int mode)
-{
-	if (mode == DEFAULT)
-		signal(CTRL_C, sig_handler_def);
-	else if (mode == CHILDS)
-		signal(CTRL_C, sig_handler_childs);
-	signal(CTRL_SLASH, sig_handler_def);
-	return (1);
-}
-
-void	create_heredoc(char *delimiter, char *path)
-{
-	char	*str;
-	int		fd;
-
-	fd = open(path, O_CREAT | O_WRONLY | O_TRUNC, 0666);
-	str = readline("> ");
-	while (str && ft_strcmp(delimiter, str) != 0)
-	{
-		str = ft_strjoin(str, "\n");
-		ft_putstr_fd(str, fd);
-		free(str);
-		str = readline("> ");
-	}
-	close(fd);
-	free(str);
-	init_signals(DEFAULT);
-}
-
-void	do_heredoc(int i, t_redir *aux)
-{
-	char	*num;
-	char	*path;
-
-	num = ft_itoa(i);
-	path = ft_strjoin("/tmp/minishell", num);
-	free(num);
-	create_heredoc(aux->path, path);
-	free(aux->path);
-	aux->path = ft_strdup(path);
-	free(path);
-}
-
 void	heredoc(t_data *data)
 {
+	int	fd;
 	t_redir *aux;
-	int		i;
-
-	i = 0;
-	while (data)
-	{
-		aux = data->redir;
-		while (aux)
-		{
-			if (aux->type == D_MINOR)
-			{
-				do_heredoc(i, aux);
-				i++;
-			}
-			aux = aux->next;
-		}
-		data = data->next;
-	}
-	//return (0);
+	int	i;
+	char	*str;
 }
 
 void	handle_redir(t_data *data)
 {
 	int fd;
-	t_redir *redir = data->redir;
+	t_redir *redir
 
+	redir = data->redir;
+
+	if (redir->type == D_MAJOR)
+		heredoc(data);
 	while (redir != NULL)
 	{
-		if (redir->type == MAJOR && redir->next == NULL)
+		if (redir->type == MAJOR)
 		{
 			fd = open(redir->path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 			if (fd == -1)
@@ -164,10 +92,6 @@ void	handle_redir(t_data *data)
 				perror("dup2");
 				exit(EXIT_FAILURE);
 			}
-		}
-		else if (redir->type == D_MINOR)
-		{
-			heredoc(data);
 		}
 		redir = redir->next;
 		close(fd);
