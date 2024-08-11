65afb58ec6e406bc32313645001d9b95497ac722
KevinPortatil <kkevinlunaa95@gmail.com>
2024-08-11 21:39:09 +0200

    clean proj

--- minishell/minishell.c
+++ minishell/minishell.c
@@ -6,7 +6,7 @@
 /*   By: kevin <kevin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 10:01:34 by kluna-bo          #+#    #+#             */
-/*   Updated: 2024/08/11 20:52:43 by kevin            ###   ########.fr       */
+/*   Updated: 2024/08/11 21:36:24 by kevin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,7 @@ int	main(int argc, char *argv[], char *env[])
 		if (input && *input)
 			add_history(input);
 		handle_input(env, &d, &mat, input);
-		// print_data(d);
+		print_data(d);
 		if (ft_is_exit(d) == 1)
 			break ;
 		(handle_env_file(&d), ft_free_resources(&d, &input, &mat));
